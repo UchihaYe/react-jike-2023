900cb77dd8bef8eed7801b7b2725469564360279
王叶盛 <2667009323@qq.com>
2023-12-17 21:17:43 +0800

    token注入

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 // axios的封装处理
 import axios from "axios";
+import { getToken } from "./token";
 // 1.根域名配置
 // 2.超时时间
 // 3.请求拦截器 | 响应拦截器
@@ -9,21 +10,31 @@ const request = axios.create({
 });
 
 // 添加请求拦截器
-request.interceptors.request.use((config)=> {
-    return config
-  }, (error)=> {
-    return Promise.reject(error)
-})
+request.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // 添加响应拦截器
-request.interceptors.response.use((response)=> {
+request.interceptors.response.use(
+  (response) => {
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
-    return response.data
-  }, (error)=> {
+    return response.data;
+  },
+  (error) => {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
-    return Promise.reject(error)
-})
+    return Promise.reject(error);
+  }
+);
 
 export { request };
